24932af61f6bbba9fe55dd310d05c47e4e05b6c6
fabiodabahia <fabio.da.bahia@hotmail.com>
2025-07-17 15:39:02 -0300

    004

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,3 @@
 
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/main/{value}',[MainController::class, 'index']);
